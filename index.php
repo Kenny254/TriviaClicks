3e74609e4332c68ea47e03f6507b4be9611cfe1e
Lawrence94 <L.agbani@yahoo.com>
2015-12-22 11:22:08 +0100

    changed environment from dev to production in order to switch off error reporting

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ require_once __DIR__ . '/vendor/facebook-php-sdk/src/Facebook/autoload.php';
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
